da1a2bfb988122d2cbd448b6d87871a74e59a7ab
BlueCitron <sds901234@naver.com>
2019-09-09 00:08:28 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="text-center my-5">할 일 목록</h1>
+
+    <div class="row justify-content-center">
+      <div class="input-group mb-3 w-75">
+        <input
+          v-model="todo"
+          v-on:keyup.enter="addTodo()"
+          type="text"
+          class="form-control todo-input"
+          placeholder="오늘의 할 일은 무엇인가요?"
+        >
+      </div>
+    </div>
+
+    <ul class="nav nav-tabs">
+      <li class="nav-item" v-for="item in ['TODO', 'PROGRESS', 'COMPLETED']">
+        <a
+        :class="{ 'nav-link': true, 'active': tab === item }"
+        href="#"
+        @click="changeTab(item)">{{ item }}</a>
+      </li>
+    </ul>
+
+    <template v-if="tab === 'TODO'">
+      <List
+      listName="TODO"
+      :list="todos"
+      :startAction="changeState"
+      :deleteAction="deleteTodo"
+      ></List>
+    </template>
+    <template v-else-if="tab === 'PROGRESS'">
+      <List
+      listName="PROGRESS"
+      :list="progresses"
+      :startAction="changeState"
+      :deleteAction="deleteTodo"
+      ></List>
+    </template>
+    <template v-else-if="tab === 'COMPLETED'">
+      <List
+      listName="COMPLETED"
+      :list="completeds"
+      :startAction="changeState"
+      :deleteAction="deleteTodo"
+      ></List>
+    </template>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import moment from 'moment'
+import List from '@/components/List.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
+    List,
+  },
+  data () {
+    return  {
+      todo: '',
+      todoSeq: 1,
+      todos: [],
+      progresses: [],
+      completeds: [],
+      tab: 'TODO',
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    addTodo () {
+      const todoName = this.todo
+      const todo = {
+        seq: this.todoSeq++,
+        name: todoName,
+        date: moment().format('YYYY-MM-DD hh:mm:ss.SSS')
+      }
+      this.todos.push(todo)
+    },
+    changeTab (tab) {
+      this.tab = tab
+    },
+    changeState (tab, todo) {
+      alert(tab)
+      console.log(tab)
+    },
+    deleteTodo (tab, todo) {
+
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="row my-3" v-for="item in list" :key="item.seq">
+      <div class="col-md-8 offset-md-2">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ item.name }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ item.date }}</h6>
+            <a
+            href="#"
+            class="btn btn-outline-success btn-sm mx-1">START</a>
+            <a
+            href="#"
+            class="btn btn-outline-danger btn-sm mx-1">DELETE</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listName: String,
+    list: Array,
+    startAction: Function,
+    deleteAction: Function,
+  }
+}
+</script>
